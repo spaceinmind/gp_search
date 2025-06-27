0362c2a0b842ed6383ff8cdb7683b4c51c32c173
Simon C.-C. Ho <50296436+spaceinmind@users.noreply.github.com>
2025-06-27 13:59:28 +1000

    Add files via upload

---
+++ get-xprof-filenames.py
@@ -0,0 +1,25 @@
+
+import sys
+
+file = open('xprof_output.txt', 'r')
+ 
+string_to_cr = ""
+    
+while True:
+
+    # read a char
+    char = file.read(1) 
+
+    if not char:
+        sys.exit()
+    
+    if ord(char) != 10:
+        string_to_cr = str(string_to_cr) + str(char)
+        #print(char, ord(char), string_to_cr)
+    else:
+        if string_to_cr[0:8] == "filename":
+            print(string_to_cr)
+        string_to_cr = ""
+    
+file.close()
+
